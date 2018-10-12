27b34331bd93f891d57edae97b7ae32ed88c8270
joeyxia <joeyxia@tencent.com>
2018-10-12 14:56:27 +0800

    AutoContrast 增加clip逻辑

--- AutoContrast.py
+++ AutoContrast.py
@@ -1,19 +1,31 @@
-# TODO: need clip min and max
-
 from PIL import Image
 import numpy as np
 
 src = Image.open("./i.png")
-src_ndarray = np.array(src)
+src_array = np.array(src)
 
-min_r = np.min(src_ndarray[:,:,0])
-min_g = np.min(src_ndarray[:,:,0])
-min_b = np.min(src_ndarray[:,:,0])
-max_r = np.max(src_ndarray[:,:,0])
-max_g = np.max(src_ndarray[:,:,0])
-max_b = np.max(src_ndarray[:,:,0])
+percent = 0.001
+src_r_list = src_array[:,:,0].flatten()
+src_g_list = src_array[:,:,1].flatten()
+src_b_list = src_array[:,:,2].flatten()
+
+src_r_list = np.sort(src_r_list)
+src_g_list = np.sort(src_g_list)
+src_b_list = np.sort(src_b_list)
+min_index = int(len(src_r_list) * percent)
+max_index = min(int(len(src_r_list) * (1.0 - percent)), len(src_r_list) - 1)
+
+min_r = src_r_list[min_index]
+min_g = src_g_list[min_index]
+min_b = src_b_list[min_index]
+max_r = src_r_list[max_index]
+max_g = src_g_list[max_index]
+max_b = src_b_list[max_index]
 min_value = min(min_r, min_g, min_b)
 max_value = max(max_r, max_g, max_b)
+print(min_r, max_r)
+print(min_g, max_g)
+print(min_b, max_b)
 
 table = []
 for i in range(256):
@@ -24,11 +36,11 @@ for i in range(256):
     else:
         table.append((i - min_value) * 255 // (max_value - min_value))
 
-for i in range(src_ndarray.shape[0]):
-    for j in range(src_ndarray.shape[1]):
-        src_ndarray[i][j][0] = table[src_ndarray[i][j][0]]
-        src_ndarray[i][j][1] = table[src_ndarray[i][j][1]]
-        src_ndarray[i][j][2] = table[src_ndarray[i][j][2]]
+for i in range(src_array.shape[0]):
+    for j in range(src_array.shape[1]):
+        src_array[i][j][0] = table[src_array[i][j][0]]
+        src_array[i][j][1] = table[src_array[i][j][1]]
+        src_array[i][j][2] = table[src_array[i][j][2]]
 
-result = Image.fromarray(src_ndarray)
-result.save("result.jpg")
+result = Image.fromarray(src_array)
+result.save("./result.jpg")
